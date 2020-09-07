41910c2df178a9200b4f290ae6e0efdbbad5145f
Taichi FURUHASHI <mapconcierge@gmail.com>
2020-09-07 22:26:58 +0900

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-overflow/`, // This path is subpath of your hosting https://domain/portfolio
-  authorName: 'Anubhav',
-  heading: 'Web Developer',
+  authorName: 'FuruhashiLab.',
+  heading: 'Addicted Mapper',
   // social
   socialLinks: [
     {
